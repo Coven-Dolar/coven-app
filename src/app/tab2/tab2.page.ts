aaa1fbfbc0c965c4504bade468941b011e323b70
angel <angel@alphacode.com.br-at-503688510464>
2020-02-25 10:33:19 -0300

    integracion con backend

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RequestAPI} from '../services/RequestAPI';
 
 @Component({
   selector: 'app-tab2',
@@ -6,48 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  datos = [
-    {
-      nombre: 'HSM1 CIF',
-      precio_venta: '310,00',
-      precio_compra: '250,00',
-      par: 'USD/T',
-      movilidad: '1.5'
-    },
-    {
-      nombre: 'HSM1 EXWORK',
-      precio_venta: '310,00',
-      precio_compra: '250,00',
-      par: 'USD/T',
-      movilidad: '1.5'
-    },
-    {
-      nombre: 'PLASTICO',
-      precio_venta: '310,00',
-      precio_compra: '250,00',
-      par: 'USD/T',
-      movilidad: '1.5'
-    },
-    {
-      nombre: 'PETRO',
-      precio_venta: '4428329,36',
-      precio_compra: '4328329,36',
-      par: 'BS/PTR',
-      movilidad: '1.5'
-    },
-    {
-      nombre: 'DOLAR',
-      precio_venta: '76550,24',
-      precio_compra: '73550,24',
-      par: 'BS/USD',
-      movilidad: '1.5'
-    },
-  ];
+  datos = [];
   today = new Date();
-  constructor() {
+  constructor(
+      private http: RequestAPI
+  ) {
     setInterval( () => {
       this.today = new Date();
     }, 1000);
+  }
+
+  async ionViewDidEnter() {
+    this.http.get('indicadores/mercados/nacionales/').subscribe( (response: any) => {
+      this.datos = response;
+    });
   }
 
 }
