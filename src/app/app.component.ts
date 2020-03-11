b56e23e091e0009ce5170c47ff580a05c6c40412
angel <angel@alphacode.com.br-at-503688510464>
2020-03-11 20:39:58 -0300

    token notificações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import {FirebaseX} from '@ionic-native/firebase-x/ngx';
 import {FCM} from '@ionic-native/fcm/ngx';
 import {RequestAPI} from './services/RequestAPI';
 
